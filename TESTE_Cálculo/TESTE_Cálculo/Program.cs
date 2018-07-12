e8aec39d9ed46925d6d4e73a1e02ddeaa475dbc4
Bruno Gino <38846449+BrunoGino@users.noreply.github.com>
2018-07-11 22:36:46 -0300

    Update Program.cs

--- TESTE_Cálculo/TESTE_Cálculo/Program.cs
+++ TESTE_Cálculo/TESTE_Cálculo/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,57 +8,55 @@ namespace TESTE_Cálculo
 {
     class Program
     {
-        /*As expressões Math.Pow abaixo são representações da potênciação na linguagem C#, que são respectivamente:
-          Math.Pow(número a ser elevado, grau da potência)*/
+        /*The Math.Pow expressions below are representations of potenciation in the C# language, whose respectively are:
+          Math.Pow(number who's going to be potentiated, potentiation number)*/
 
-        //Função que descreve o padrão de equações polinomiais de segundo grau
+        //Function wich describes the second-degree polynomial equations pattern 
          static double PolinomialGrau2(double a,double b,double c,double exp1,double const1,double const2, int exp2 = 1)
         {
             return((const1*(Math.Pow(a,exp1)))+(const2*(Math.Pow(b,exp2)))+c);
         }
-        //Função que descreve o padrão de equações polinomiais de terceiro grau
+        //Function wich describes the third-degree polynomial equations pattern 
         static double PolinomialGrau3(double a,double b,double c,double d,double const1,double const2,double const3,double exp1,double exp2,int exp3=1)
         {
             return ((const1*(Math.Pow(a,exp1)))+(const2*(Math.Pow(b,exp2)))+(const3*(Math.Pow(c,exp3)))+d);
         }
-        //Função que descreve o padrão de equações polinomiais de quarto grau
+    	//Function wich describes the fourth-degree polynomial equations pattern
         static double PolinomialGrau4(double a,double b,double c,double d,double e,double const1,double const2,double const3,double const4,double exp1,double exp2,double exp3,int exp4=1)
         {
             return ((const1*(Math.Pow(a,exp1)))+(const2*(Math.Pow(b,exp2)))+(const3*(Math.Pow(c,exp3)))+(const4*(Math.Pow(d,exp4)))+e);
         }
-        //Função que descreve o padrão da derivada de uma equação do segundo grau
+	//Function that describes the derivative pattern of a second degree equation
         static double DerivadaFunçãoPolinomialGrau2(double a,double b, double exp1,double const1,double const2, int exp2 = 1)
         {
 
             return(exp1 * (const1 *( Math.Pow(a, (exp1 - 1))))) + (exp2 * (const2 * (Math.Pow(b, (exp2-1)))));
         }
-        //Função que descreve o padrão da derivada de uma equação do terceiro grau
+        // Function that describes the derivative pattern of a third degree equation
         static double DerivadaFunçãoPolinomialGrau3(double a,double b,double c,double const1,double const2,double const3,double exp1,double exp2,int exp3=1)
         {
             return ((exp1*(const1*(Math.Pow(a,(exp1-1)))))+(exp2*(const2*(Math.Pow(b,(exp2-1)))))+(exp3*(const3*(Math.Pow(c,(exp3-1))))));
         }
-        //Função que descreve o padrão da derivada de uma equação do quarto grau
+        // Function that describes the derivative pattern of a fourth degree equation
         static double DerivadaFunçãoPolinomialGrau4(double a,double b,double c,double d,double const1,double const2,double const3,double const4,double exp1,double exp2,double exp3,double exp4=1)
         {
             return ((exp1*(const1*(Math.Pow(a,(exp1-1)))))+(exp2*(const2*Math.Pow(b,(exp2-1))))+(exp3*(const3*(Math.Pow(c,exp3-1))))+(exp4*(const4*(Math.Pow(d,(exp4-1))))));
         }
 
         static void Main()
-        {     
-            //Variáveis para a realização dos cálculos
-            double E; //épsilon
-            int Kmax; //Número máximo de iterações
-            int K=0; //Contador de iterações
-            double Xo; //X inicial
-            double erro; //Variável do Erro
-            double xAtual;
-            double const1,const2,const3,const4; //Constantes das funções polinomiais e derivadas  
-            double exp1,exp2,exp3;   //Expoentes das funções polinomias e derivadas
-            double polinomial; //Variável a receber o padrão da equação polinomial
-            double derivada;   //Variável a receber o padrão da derivada
-            double c,d,e;    //Constantes C,D e E de equações polinomiais
-
-            //Instruções do programa
+        {                
+            double E; //epsilon
+            int Kmax; //Maximum number of iterations
+            int K=0; //Iteration counter
+            double Xo; //Initial X
+            double erro; //Error variable
+            double xAtual; //The current value of X 
+            double const1,const2,const3,const4; //Constants of polynomial and derived functions  
+            double exp1,exp2,exp3;   //Exponents of polynomial and derived functions
+            double polinomial; //Variable to receive the polynomial equation pattern
+            double derivada;   // Variable to receive the derivative pattern
+            double c,d,e;    //C, D and E polynomial equations constants
+            //Program instructions
             Console.WriteLine("\tMétodo de Newton-Raphson\n");
             Console.WriteLine("________________________________________________________________________________________________________________");
             Console.WriteLine("\tInstruções:");
@@ -69,7 +67,7 @@ namespace TESTE_Cálculo
                 "\nEntão a equação de terceiro grau no programa será formada da seguinte maneira:X^3+ 0*X^n -9*X+3\n");
             Console.WriteLine("-A equação será formada passo a passo, portanto, fique tranquilo, o programa irá receber os valores separadamente!\n");
             Console.WriteLine("________________________________________________________________________________________________________________");
-            //Inicia o passo a passo
+            //Starts the step-by-step
             Console.Write("\nInforme o valor de Épsilon (ε): ");
             E = Double.Parse(Console.ReadLine());
             Console.Write("Insira o número máximo de iterações: ");
@@ -77,10 +75,10 @@ namespace TESTE_Cálculo
             Console.Write("Informe o grau da função: ");
             exp1 = Double.Parse(Console.ReadLine());        
             
-            //Irá denominar o padrão de equação polinomial e derivada pelo grau da função
+            //It will determine the polynomial equation pattern by the function's degree
             switch (exp1)
             {
-                case 2://Caso seja de segundo grau
+                case 2://If it is a second-degree equation
                     Console.Write("Informe o valor do chute: ");
                     Xo = Double.Parse(Console.ReadLine());
                     Console.Write("Insira o valor da primeira constante: ");
@@ -89,7 +87,7 @@ namespace TESTE_Cálculo
                     const2 = Double.Parse(Console.ReadLine());
                     Console.Write("Insira o valor de C: ");
                     c= Double.Parse(Console.ReadLine());
-                    //Denomina como padrão de equação do segundo grau
+                    //Denominates the standard equation of the second degree
                     polinomial=PolinomialGrau2(Xo, Xo, c, exp1, const1, const2);
                     derivada=DerivadaFunçãoPolinomialGrau2(Xo, Xo, exp1, const1, const2);
 
@@ -98,7 +96,7 @@ namespace TESTE_Cálculo
                     xAtual = xAtual - erro;
                     K++;
                     Console.WriteLine("\n");
-                    //Primeira iteração (grau 2)
+                    //First iteration (Second-degree)
                     Console.WriteLine("Primeira iteração");
                     Console.WriteLine("Valor da função polinomial no x" + K + " ={0}", polinomial);
                     Console.WriteLine("Valor da derivada da função no x" + K + "={0}", derivada);
@@ -106,7 +104,7 @@ namespace TESTE_Cálculo
                     Console.WriteLine("Valor do erro = {0}", (polinomial / derivada));
                     Console.WriteLine("Nº de iterações={0}", K);
                     Console.WriteLine("\n");
-                    //Inicia o loop
+                    //Initiates the loop
                     do
                     {
                         polinomial = PolinomialGrau2(xAtual, xAtual, c, exp1, const1, const2);
@@ -124,7 +122,7 @@ namespace TESTE_Cálculo
                     } while (erro >= E && Kmax > K);//Condição do loop e fim do loop
 
                     break;
-                case 3://Caso seja de terceiro grau
+                case 3://If it is a third-degree equation
                     Console.Write("Insira o valor do chute: ");
                     Xo = Double.Parse(Console.ReadLine());
                     Console.Write("Insira o valor da primeira constante: ");
@@ -137,7 +135,7 @@ namespace TESTE_Cálculo
                     exp2 = Double.Parse(Console.ReadLine());
                     Console.Write("Insira o valor de D: ");
                     d = Double.Parse(Console.ReadLine());
-                    //Denomina como padrão de equação do terceiro grau
+                   //Denominates the standard equation of the third degree
                     polinomial = PolinomialGrau3(Xo, Xo, Xo, d, const1, const2, const3, exp1, exp2);
                     derivada=DerivadaFunçãoPolinomialGrau3(Xo, Xo, Xo, const1, const2, const3, exp1, exp2);
 
@@ -146,7 +144,7 @@ namespace TESTE_Cálculo
                     xAtual = xAtual - erro;
                     K++;
                     Console.WriteLine("\n");
-                    //Primeira iteração (grau 3)
+                    //First iteration (Third degree)
                     Console.WriteLine("Primeira iteração");
                     Console.WriteLine("Valor da função polinomial no x" + K + " ={0}", polinomial);
                     Console.WriteLine("Valor da derivada da função no x" + K + "={0}", derivada);
@@ -171,7 +169,7 @@ namespace TESTE_Cálculo
                     } while (erro >= E && Kmax > K);
 
                     break;
-                case 4://Caso seja de quarto grau
+                case 4://If it is a fourth-degree equation
                     Console.Write("Insira o valor do chute: ");
                     Xo = Double.Parse(Console.ReadLine());
                     Console.WriteLine("Insira o valor da primeira constante: ");
@@ -188,7 +186,7 @@ namespace TESTE_Cálculo
                     exp3 = Double.Parse(Console.ReadLine());
                     Console.Write("Insira o valor de E: ");
                     e = Double.Parse(Console.ReadLine());
-                    //Denomina como padrão de equação do quarto grau
+                    //Denominates the standard equation of the fourth degree
                     polinomial = PolinomialGrau4(Xo,Xo , Xo, Xo, e, const1, const2, const3, const4, exp1, exp2, exp3);
                     derivada=DerivadaFunçãoPolinomialGrau4(Xo, Xo, Xo, Xo, const1, const2, const3, const4, exp1, exp2, exp3);
 
@@ -198,7 +196,7 @@ namespace TESTE_Cálculo
                     K++;
 
                     Console.WriteLine("\n");
-                    //Primeira iteração (grau 4)
+                    //First iteration (Fourth degree)
                     Console.WriteLine("Primeira iteração");
                     Console.WriteLine("Valor da função polinomial no x" + K + " ={0}", polinomial);
                     Console.WriteLine("Valor da derivada da função no x" + K + "={0}", derivada);
@@ -223,7 +221,7 @@ namespace TESTE_Cálculo
                     } while (erro >= E && Kmax > K);
 
                     break;
-                default://Opção inválida
+                default://Invalid option
                     Console.WriteLine("Grau de função inválido !! Insira no máximo até o quarto grau !!");
                     break;
             }            
